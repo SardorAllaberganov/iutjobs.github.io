c2b50fdcc95c51674bb5a1bac7846fedc2b5326a
SardorAllaberganov <sardorallaberganov96@gmail.com>
2019-05-23 00:59:25 +0500

    personal page

--- data/js/index.js
+++ data/js/index.js
@@ -42,6 +42,37 @@ $(document).ready(function(){
           count++;
       }
     });
-    $("#filter-count").text("Number of results " + count);
+    if($("#filter").val() == ""){
+      $("#filter-count").hide();
+    }
+    else{
+      $("#filter-count").show();
+      $("#filter-count").text("Number of results " + count);
+    }
+  });
+});
+
+$(document).ready(function(){
+  $("#filterPersonal").keyup(function(){
+    // Retrieve the input field text and reset the count to zero
+    var filter = $(this).val(), count = 0;
+    // Loop through the comment list
+    $(".personalInfo div").each(function(){
+      // If the list item does not contain the text phrase fade it out
+      if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+        $(this).fadeOut();
+        // Show the list item if the phrase matches and increase the count by 1
+      } else {
+        $(this).show();
+          count++;
+      }
+    });
+    if($("#filterPersonal").val() == ""){
+      $("#filter-count").hide();
+    }
+    else{
+      $("#filterPersonal").show();
+      $("#filter-count").text("Number of results " + count);
+    }
   });
 });
